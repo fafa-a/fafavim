5bb96b8fdb7dbaf93379af54ebd397a5c8ebf063
fafa-a <fafa@DESKTOP-5229KJE>
2024-02-09 23:41:50 +0100

    feat(extra): disable coding.copilot and cmp trigger

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,49 +1,44 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  -- build = ":Copilot auth",
-  event = "VimEnter",
-  opts = {
-    panel = {
-      enabled = true,
-      auto_refresh = false,
-      keymap = {
-        jump_prev = "[[",
-        jump_next = "]]",
-        accept = "<CR>",
-        refresh = "gr",
-        open = "<M-CR>",
-      },
-    },
-    suggestion = {
-      enabled = true,
-      auto_trigger = true,
-      debounce = 75,
-      keymap = {
-        accept = false,
-        accept_word = false,
-        accept_line = false,
-        next = "<M-]>",
-        prev = "<M-[>",
-        dismiss = "<C-]>",
-      },
-    },
-    filetypes = {
-      yaml = false,
-      markdown = true,
-      help = true,
-      gitcommit = false,
-      gitrebase = false,
-      hgcommit = false,
-      svn = false,
-      cvs = false,
-      ["."] = false,
-    },
-    copilot_node_command = "node", -- Node.js version must be > 16.x
-    server_opts_overrides = {},
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "VimEnter",
+    config = function()
+      require("copilot").setup({
+        panel = {
+          enabled = false,
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = "<M-l>",
+            accept_word = false,
+            accept_line = false,
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        },
+        filetypes = {
+          yaml = false,
+          help = false,
+          gitcommit = false,
+          gitrebase = false,
+          hgcommit = false,
+          svn = false,
+          cvs = false,
+          ["."] = false,
+        },
+        copilot_node_command = "node", -- Node.js version must be > 18.x
+        server_opts_overrides = {},
+      })
+    end,
   },
   {
-    "jellydn/CopilotChat.nvim",
+    "CopilotC-Nvim/CopilotChat.nvim",
     opts = {
       mode = "split", -- newbuffer or split  , default: newbuffer
       prompts = {
