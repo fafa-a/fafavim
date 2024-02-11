cf127cded1f20efcb5892f843ab7714552ef88f5
fafa-a <fafa@DESKTOP-5229KJE>
2024-02-11 15:59:02 +0100

    feat: add harpoon

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -66,4 +66,44 @@ return {
       end,
     },
   },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local harpoon = require("harpoon")
+
+      harpoon:setup({
+        settings = {
+          save_on_toggle = true,
+        },
+      })
+
+      vim.keymap.set("n", "<leader>a", function()
+        harpoon:list():append()
+      end, { noremap = true, silent = true, desc = "[harpoon] append" })
+      vim.keymap.set("n", "<leader>hm", function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end, { noremap = true, silent = true, desc = "[harpoon] menu" })
+
+      vim.keymap.set("n", "[h", function()
+        harpoon:list():prev()
+      end, { noremap = true, silent = true, desc = "[harpoon] previous" })
+      vim.keymap.set("n", "]h", function()
+        harpoon:list():next()
+      end, { noremap = true, silent = true, desc = "[harpoon] next" })
+      vim.keymap.set("n", "<leader>h1", function()
+        harpoon:list():select(1)
+      end, { noremap = true, silent = true, desc = "[harpoon] select 1" })
+      vim.keymap.set("n", "<leader>h2", function()
+        harpoon:list():select(2)
+      end, { noremap = true, silent = true, desc = "[harpoon] select 2" })
+      vim.keymap.set("n", "<leader>h3", function()
+        harpoon:list():select(3)
+      end, { noremap = true, silent = true, desc = "[harpoon] select 3" })
+      vim.keymap.set("n", "<leader>h4", function()
+        harpoon:list():select(4)
+      end, { noremap = true, silent = true, desc = "[harpoon] select 4" })
+    end,
+  },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -14,6 +14,7 @@ return {
       "sql",
       "svelte",
       "tsx",
+      "typescript",
     },
   },
 }
