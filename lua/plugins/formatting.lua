6b259ee44a1899df0f5605e6cbcd21ea1a4f6f80
PUECH Fabien <fabien.puech@csgroup.eu>
2024-03-02 18:05:42 +0100

    feat(formatting): depends on cfg file

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -2,8 +2,32 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      javascript = { "biome" },
-      typescript = { "biome" },
+      javascript = function(bufnr)
+        return check_prettier_config(bufnr)
+      end,
+      javascriptreact = function(bufnr)
+        return check_prettier_config(bufnr)
+      end,
+      typescript = function(bufnr)
+        return check_prettier_config(bufnr)
+      end,
+      typescriptreact = function(bufnr)
+        return check_prettier_config(bufnr)
+      end,
     },
+    -- Set the log level
+    log_level = vim.log.levels.ERROR,
+    -- Conform will notify you when a formatter errors
+    notify_on_error = true,
   },
 }
+
+function check_prettier_config(bufnr)
+  local prettier_configs = { ".prettierrc", ".prettierrc.json", ".prettierrc.yaml", ".prettierrc.yml", ".prettierrc.js", "prettier.config.js" }
+  for _, config_file in ipairs(prettier_configs) do
+    if vim.fn.filereadable(config_file) == 1 then
+      return { "prettierd", "prettier" }
+    end
+  end
+  return { "biome" }
+end
