fbe23ec6a3579ce9fa199716ebcd0f346f4f05fe
fafa-a <fafa@DESKTOP-5229KJE>
2024-03-17 16:46:31 +0100

    feat(gitsign): remove signs columns

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -66,9 +66,46 @@ return {
       end,
     },
   },
-  { "lewis6991/gitsigns.nvim", event = "LazyFile", opts = {
-    numhl = true,
-  } },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "LazyFile",
+    opts = {
+      numhl = true,
+      signcolumn = false,
+      -- signs = {
+      --   add = {
+      --     hl = "GitSignsAdd",
+      --     text = "",
+      --     numhl = "GitSignsAddNr",
+      --     linehl = "GitSignsAddLn",
+      --   },
+      --   change = {
+      --     hl = "GitSignsChange",
+      --     text = "ﰣ",
+      --     numhl = "GitSignsChangeNr",
+      --     linehl = "GitSignsChangeLn",
+      --   },
+      --   delete = {
+      --     hl = "GitSignsDelete",
+      --     text = "",
+      --     numhl = "GitSignsDeleteNr",
+      --     linehl = "GitSignsDeleteLn",
+      --   },
+      --   topdelete = {
+      --     hl = "GitSignsDelete",
+      --     text = "",
+      --     numhl = "GitSignsDeleteNr",
+      --     linehl = "GitSignsDeleteLn",
+      --   },
+      --   changedelete = {
+      --     hl = "GitSignsChange",
+      --     text = "ﰣ",
+      --     numhl = "GitSignsChangeNr",
+      --     linehl = "GitSignsChangeLn",
+      --   },
+      -- },
+    },
+  },
   {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
