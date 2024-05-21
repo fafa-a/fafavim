8a19db3f60dc79ef75411b2bd48de34e466715a8
fafa-a <fafa@DESKTOP-5229KJE>
2024-05-21 22:34:41 +0200

    feat: add barbecue & troublev3

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
+    { import = "lazyvim.plugins.extras.editor.trouble-v3" },
     { import = "plugins" },
   },
   defaults = {
