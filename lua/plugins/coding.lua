209592c6b84c4cdeabce8436f16f6175f9395108
fafa-a <fafa@DESKTOP-5229KJE>
2024-06-26 22:52:54 +0200

    feat: add supermaven& convert (CSS)

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,39 +1,51 @@
 return {
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = "Copilot",
+  --   build = ":Copilot auth",
+  --   event = "VimEnter",
+  --   config = function()
+  --     require("copilot").setup({
+  --       panel = {
+  --         enabled = false,
+  --       },
+  --       suggestion = {
+  --         enabled = true,
+  --         auto_trigger = true,
+  --         debounce = 75,
+  --         keymap = {
+  --           accept = "<C-j>",
+  --           accept_word = false,
+  --           accept_line = false,
+  --           next = "<C-]>",
+  --           prev = "<C-[>",
+  --           dismiss = "<C-]>",
+  --         },
+  --       },
+  --       filetypes = {
+  --         yaml = false,
+  --         help = false,
+  --         gitcommit = false,
+  --         gitrebase = false,
+  --         hgcommit = false,
+  --         svn = false,
+  --         cvs = false,
+  --         ["."] = false,
+  --       },
+  --       copilot_node_command = "node", -- Node.js version must be > 18.x
+  --       server_opts_overrides = {},
+  --     })
+  --   end,
+  -- },
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    event = "VimEnter",
+    "supermaven-inc/supermaven-nvim",
     config = function()
-      require("copilot").setup({
-        panel = {
-          enabled = false,
+      require("supermaven-nvim").setup({
+        keymaps = {
+          accept_suggestion = "<C-j>",
+          clear_suggestion = "<C-]>",
+          accept_word = "<C-k>",
         },
-        suggestion = {
-          enabled = true,
-          auto_trigger = true,
-          debounce = 75,
-          keymap = {
-            accept = "<C-j>",
-            accept_word = false,
-            accept_line = false,
-            next = "<C-]>",
-            prev = "<C-[>",
-            dismiss = "<C-]>",
-          },
-        },
-        filetypes = {
-          yaml = false,
-          help = false,
-          gitcommit = false,
-          gitrebase = false,
-          hgcommit = false,
-          svn = false,
-          cvs = false,
-          ["."] = false,
-        },
-        copilot_node_command = "node", -- Node.js version must be > 18.x
-        server_opts_overrides = {},
       })
     end,
   },
