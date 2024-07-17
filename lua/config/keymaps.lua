fcbdcd250fb1b82530271029a7548df6edddb104
fafa-a <fafa@DESKTOP-5229KJE>
2024-07-17 23:53:31 +0200

    feat(term): handle new term for lazygit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -29,31 +29,48 @@ map("n", "<Leader>ow", "<Plug>VimwikiIndex", { silent = true })
 --   -- Put cursor on next line.
 --   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<End>", true, false, true), "n", false)
 -- end)
-
-local fterm = require("FTerm")
-map('n', '<C-`>', '<CMD>lua require("FTerm").toggle()<CR>')
-map('t', '<C-`>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
-
-
-local btop = fterm:new({
-  ft = "fterm_btop",
-  cmd = "btop",
-})
-
-local lazygit = fterm:new({
-  ft = "fterm_lazygit",
-  cmd = "lazygit",
-})
-
--- Use this to toggle btop in a floating terminal
-map("n", "<A-b>", function()
-  btop:toggle()
-end)
-
 -- Use this to toggle lazygit in a floating terminal
-map("n", "<C-g>", function()
-  lazygit:toggle()
-end)
+
+local function create_float_term()
+    local width = math.floor(vim.o.columns * 0.9)
+    local height = math.floor(vim.o.lines * 0.9)
+    local buf = vim.api.nvim_create_buf(false, true)
+    local opts = {
+        relative = "editor",
+        width = width,
+        height = height,
+        col = math.floor((vim.o.columns - width) / 2),
+        row = math.floor((vim.o.lines - height) / 2),
+        style = "minimal",
+        border = "rounded"
+    }
+    local win = vim.api.nvim_open_win(buf, true, opts)
+    return buf, win
+end
+
+local function open_lazygit()
+    local buf, win = create_float_term()
+
+    vim.fn.termopen("lazygit", {
+        on_exit = function()
+            if vim.api.nvim_buf_is_valid(buf) then
+                vim.api.nvim_buf_delete(buf, { force = true })
+            end
+            if vim.api.nvim_win_is_valid(win) then
+                vim.api.nvim_win_close(win, true)
+            end
+        end
+    })
+    vim.cmd('startinsert')
+end
+
+vim.api.nvim_create_user_command('LazyGit', open_lazygit, {})
+vim.api.nvim_set_keymap('n', '<C-g>', ':LazyGit<CR>', { noremap = true, silent = true })
+
+-- vim.api.nvim_set_keymap('n', '<C-/>', ':LazyGitTerminal<CR>', { noremap = true, silent = true })
+-- map("n", "<C-g>", function()
+--   require('toggleterm').exec('lazygit')
+-- end)
 
 -- Do not copy on paste
 map("v", "p", '"_dP')
