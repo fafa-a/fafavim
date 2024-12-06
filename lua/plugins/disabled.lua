a242fb43eea2fe10b841168bec9196d9ba4757fd
fafa-a <fafa@DESKTOP-5229KJE>
2024-12-06 23:37:53 +0100

    feat(ui): add mini statusline

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,3 +1,4 @@
 return {
   { "folke/flash.nvim", enabled = false },
+  { "nvim-lualine/lualine.nvim", enabled = false },
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -76,4 +76,42 @@ return {
       },
     },
   },
+  {
+    "echasnovski/mini.statusline",
+    version = "*",
+    config = function()
+      local MiniStatusline = require("mini.statusline")
+
+      MiniStatusline.setup({
+        content = {
+          active = function()
+            local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+            local git = MiniStatusline.section_git({ trunc_width = 40 })
+            local diff = MiniStatusline.section_diff({ trunc_width = 75 })
+            local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+            local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
+            local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+            local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+            local location = MiniStatusline.section_location({ trunc_width = 75 })
+            local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
+            local short_mode = mode:sub(1, 1)
+            return MiniStatusline.combine_groups({
+              { hl = mode_hl, strings = { short_mode } },
+              { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
+              "%<", -- Mark general truncate point
+              { hl = "MiniStatuslineFilename", strings = { filename } },
+              "%=", -- End left alignment
+              { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+              { hl = mode_hl, strings = { search, location } },
+            })
+          end,
+          inactive = function()
+            return "Inactive Window"
+          end,
+        },
+        use_icons = true,
+        set_vim_settings = true,
+      })
+    end,
+  },
 }
