005747013a47e20ae6df6737c7c351ef700c6d6d
fafa-a <fafa@DESKTOP-5229KJE>
2024-12-14 01:16:06 +0100

    style(ui): adjust dashboard padding values
    - Increase terminal section padding from 1 to 4
    - Increase keys section padding from 1 to 2

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -173,9 +173,9 @@ return {
             section = "terminal",
             cmd = "/opt/shell-color-scripts/colorscript.sh -e square",
             height = 5,
-            padding = 1,
+            padding = 4,
           },
-          { section = "keys", gap = 1, padding = 1 },
+          { section = "keys", gap = 1, padding = 2 },
           { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
           { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
           {
